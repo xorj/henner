3ef6b3323dd4073ba14432d81ffba140b6374957
Jorge Vinícius <jorge.santos@dcomp.ufs.br>
2022-10-23 22:53:58 -0300

    fix: simplifica listagens de produtos

--- src/components/LinksProducts.vue
+++ src/components/LinksProducts.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="hidden items-center md:flex h-full">
-    <router-link to="/produtos" class="text-dark flex items-center px-4 h-full">
-      <q-icon name="trending_up" color="primary" size="1.5rem" class="mr-1" />
-      <p>Mais Vendidos</p>
-    </router-link>
-    <router-link to="/produtos" class="text-dark flex items-center px-4  h-full">
-      <q-icon name="interests" color="dark" size="1.5rem" class="mr-1" />
-      <p>Categorias</p>
-    </router-link>
-    <router-link to="/produtos" class="text-dark flex items-center px-4  h-full">
-      <p>Promoções</p>
-    </router-link>
-  </div>
-  <div class="flex md:hidden items-center h-full">
+  <div class="flex items-center h-full">
     <router-link to="/produtos" class="text-dark flex items-center px-4 h-full">
       <q-icon name="sell" color="primary" size="1.5rem" class="mr-1" />
       <p>Ver produtos</p>
