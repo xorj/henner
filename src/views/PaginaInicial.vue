4c88e7657dbc3bab5f0cbd44bd37a5830091263e
Jorge Vinícius <jorge.santos@dcomp.ufs.br>
2022-10-29 02:46:56 -0300

    remove página inicial

--- src/views/PaginaInicial.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <main class="px-4 md:px-10 flex justify-center mt-20 mb-48"></main>
-</template>
-<script setup>
-import { Form, Field } from "vee-validate";
-import { useStore } from "vuex";
-</script>
