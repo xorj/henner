2b2b9a3a09f28644c61deb78bb2bca6bf46d89ca
Jorge Vinícius <jorge.santos@dcomp.ufs.br>
2022-10-29 17:11:39 -0300

    fix: lógica para acessar carrinho

--- src/components/CartLink.vue
+++ src/components/CartLink.vue
@@ -1,6 +1,17 @@
 <template>
-  <router-link to="/carrinho" class="text-dark flex items-center px-4 border-x-2 h-full">
+  <router-link
+    :to="userLogged ? '/carrinho' : '/login'"
+    class="text-dark flex items-center px-4 border-x-2 h-full"
+  >
     <q-icon name="shopping_cart" color="primary" size="1.5rem" class="mr-1" />
     <p>Carrinho</p>
+    <q-tooltip v-if="!userLogged"> Logue para acessar o carrinho </q-tooltip>
   </router-link>
 </template>
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const userLogged = computed(() => !!store.state.user.token);
+</script>
